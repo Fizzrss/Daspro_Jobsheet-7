8e1c87ade19705ae2aae141210ce536a808ee375
bintang.panca007@gmail.com <bintang.panca007@gmail.com>
2024-10-11 07:22:17 +0700

    Percobaan1Modif

--- SiakadFor09.java
+++ SiakadFor09.java
@@ -6,6 +6,7 @@ public class SiakadFor09 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         
         for (int i = 1; i <= 10; i++){
             System.out.println("Masukkan nilai mahasiswa ke- " + i + ": ");
@@ -15,8 +16,16 @@ public class SiakadFor09 {
             } if (nilai < terendah){
                 terendah = nilai;
             }
+
+            if (nilai >= 60){
+                lulus++;
+            }else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiwa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiwa yang tidak lulus: " + tidakLulus);
     }
 }
