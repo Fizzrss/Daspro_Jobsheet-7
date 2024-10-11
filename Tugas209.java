f2d236724e65fb3fca109499e4fc1ec0a601d58b
bintang.panca007@gmail.com <bintang.panca007@gmail.com>
2024-10-11 07:51:55 +0700

    jobsheet 7

---
+++ Tugas209.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Tugas209 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int jenis, durasi, total = 0;
+        
+        do {
+            System.out.println("Masukkan jenis kendaraan (1 mobil, 2 motor, 0 keluar)");
+            jenis = input.nextInt();
+        
+            if (jenis == 1 || jenis == 2){
+                System.out.println("Masukkan durasi parkir (jam): ");
+                durasi = input.nextInt();
+                
+                if (durasi > 5){
+                    total += 12500;
+                } else {
+                    if (jenis == 1){
+                        total += durasi * 3000;
+                    } else if (jenis == 2){
+                        total += durasi*2000;
+                    }
+                }
+            }
+        } while (jenis != 0);
+        System.out.println("Total pembayaran parkir dari beberapa kedaraan: " + total);
+        input.close();
+    }
+}
+
